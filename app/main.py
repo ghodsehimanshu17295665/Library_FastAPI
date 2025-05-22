b9bb770189468bdd376e9ed0d01d60335c95967d
ghodsehimanshu17295665 <himanshughodse@gmail.com>
2025-05-22 23:18:37 +0530

    add database configuration and SQLAlchemy models

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 
+from app.routers import author, category, user
+
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI + PostgreSQL"}
+
+
+# include router
+app.include_router(user.router)
+app.include_router(author.router)
+app.include_router(category.router)
