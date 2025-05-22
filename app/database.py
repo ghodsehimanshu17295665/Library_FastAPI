ee6071903d7e700362a8d97772d032f909d157f2
ghodsehimanshu17295665 <himanshughodse@gmail.com>
2025-05-22 23:17:10 +0530

    add database configuration

--- app/database.py
+++ app/database.py
@@ -15,3 +15,12 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+
+# Dependency to get DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
