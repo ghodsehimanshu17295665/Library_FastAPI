5cbb6ea81cc228527984574744786edd5ea3b095
Himanshu Ghodse <himanshughodse@gmail.com>
2025-05-26 13:13:12 +0530

    Initial Commit

--- app/auth.py
+++ app/auth.py
@@ -8,10 +8,15 @@ from sqlalchemy.orm import Session
 
 from app import database, models
 
-# JWT Config
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# JWT Config from .env
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 
 # Password hashing
--- app/database.py
+++ app/database.py
@@ -1,8 +1,8 @@
 import os
-
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from urllib.parse import quote_plus
 
 load_dotenv()
 
@@ -10,14 +10,20 @@ load_dotenv()
 class Base(DeclarativeBase):
     pass
 
+# Extract and encode DB credentials
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD"))
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME")
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 
-# Dependency to get DB session
 def get_db():
     db = SessionLocal()
     try:
